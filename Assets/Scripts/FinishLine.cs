b890c69b940609814e81908c8e7bd642cee63ff2
Louise Collins <louisecollins1971@icloud.com>
2021-10-20 18:46:11 +0100

    removed additional collider as did not resolve issue

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    [SerializeField] float loadDelay = 1f;
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == ("Player"))
+        if (other.tag == "Player")
         {
-            Debug.Log("You finished!");
+            Invoke("ReloadScene", loadDelay);
         }
+    }
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
