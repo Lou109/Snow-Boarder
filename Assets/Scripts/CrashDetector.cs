ba29315062ac70443ebcd0ec46820c55125984b4
Louise Collins <louisecollins1971@icloud.com>
2021-10-21 18:33:42 +0100

    Issue solved with latest version of Unity

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] float loadDelay = 1f;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Ground")
         {
